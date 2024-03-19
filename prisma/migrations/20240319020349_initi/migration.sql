5fc82f2e4f0a59edd55b9222f221701f34e3a45d
RickMalv <ricardo.alvarez362@gmail.com>
2024-03-19 03:02:19 -0300

    pendiente empezar el componente formulario

---
+++ prisma/migrations/20240319020349_initi/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL
+);
